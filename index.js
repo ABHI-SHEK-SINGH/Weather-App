31a7fcb55d9813385856dfbe62f3424e7d5418da
ABHI-SHEK-SINGH <abhishekbtp05@gmail.com>
2022-07-19 18:29:06 +0530

    remaning css js html code

--- index.js
+++ index.js
@@ -1,2 +1,100 @@
-a4c14dd18310d247f41b9f2ab703eead
-https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+const weatherApi ={
+    key:"a4c14dd18310d247f41b9f2ab703eead",
+    baseUrl: "https://api.openweathermap.org/data/2.5/weather"
+}
+
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+const searchInputBox = document.getElementById('input-box');
+searchInputBox.addEventListener('keypress',(event) => {
+
+    if(event.keyCode == 13){
+    console.log(searchInputBox.value);
+    getWeatherReport(searchInputBox.value)
+    document.querySelector('.weather-body').style.display = "block";
+    }
+});
+
+// weather report
+function getWeatherReport(city){
+    fetch(`${weatherApi.baseUrl}?q=${city}&appid=${weatherApi.key}&units=metric`)
+    .then(weather => {
+        return weather.json();
+    }).then(showWeatherReport);
+}
+
+// show weather
+function showWeatherReport(weather){
+    console.log(weather);
+    let city = document.getElementById('city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    let temperature = document.getElementById('temp');
+    temperature.innerHTML = `${Math.round(weather.main.temp)}&deg;C`;
+
+    let minMaxTemp = document.getElementById('min-max')
+    minMaxTemp.innerHTML = `${Math.floor(weather.main.temp_min)}&deg;C (min)/${Math.ceil(weather.main.temp_max)}&deg;C(max)`;
+
+    let weatherType = document.getElementById('weather');
+    weatherType.innerText = `${weather.weather[0].main}`;
+
+    let date = document.getElementById('date');
+    let todayDate = new Date();
+    date.innerText = dateManage(todayDate);
+
+    if(weatherType.textContent == 'Clear') {
+        document.body.style.backgroundImage = "url('images/1.jpg')";
+        
+    } else if(weatherType.textContent == 'Clouds') {
+
+        document.body.style.backgroundImage = "url('images/3.jpg')";
+        
+    } else if(weatherType.textContent == 'Haze') {
+
+        document.body.style.backgroundImage = "url('images/2.jpg')";
+        
+    }     else if(weatherType.textContent == 'Rain') {
+        
+        document.body.style.backgroundImage = "url('images/4.jpg')";
+        
+    } else if(weatherType.textContent == 'Snow') {
+        
+        document.body.style.backgroundImage = "url('images/6.jpg')";
+    
+    } else if(weatherType.textContent == 'Thunderstorm') {
+    
+        document.body.style.backgroundImage = "url('images/5.jpg')";
+
+    }
+}
+// date function
+
+function dateManage(dateArg) {
+
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    let year = dateArg.getFullYear();
+    let month = months[dateArg.getMonth()];
+    let date = dateArg.getDate();
+    let day = days[dateArg.getDay()];
+
+    return `${date} ${month} (${day}), ${year}`;
+}
+
+// moving letter function
+var i =0;
+var text = 'Weather App';
+
+
+function typing() {
+    if(i<text.length){
+        document.getElementById("texts").innerHTML += text.charAt(i);
+        i++;
+       setTimeout(typing,20);
+  
+
+    }
+}
+typing();
